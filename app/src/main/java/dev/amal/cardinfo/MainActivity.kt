4956fd7e9bbc13039c86a55f417fd4dc0af46446
JustAmal <amalkrasavchik@gmail.com>
2022-12-21 12:15:52 +0600

    initial commit

---
+++ app/src/main/java/dev/amal/cardinfo/MainActivity.kt
@@ -0,0 +1,17 @@
+package dev.amal.cardinfo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import dev.amal.cardinfo.ui.theme.CardInfoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CardInfoTheme {
+
+            }
+        }
+    }
+}
